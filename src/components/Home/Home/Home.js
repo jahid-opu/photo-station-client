bcf34ed21e7e1a121fe3d31e7edb47a8da98e39a
jahid-opu <ziopu05@gmail.com>
2021-08-19 10:46:02 +0600

    first commit

---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderMain from '../HeaderMain/HeaderMain';
+import Services from '../Services/Services';
+
+const Home = () => {
+    return (
+        <div>
+            <HeaderMain/>
+            <Services/>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Home/Services/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <section className="py-5">
+            <h2 className="text-center">Services</h2>
+        </section>
+    );
+};
+
+export default Services;
